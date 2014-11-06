772f8cb351aae8c5d845d98f5a5222efea1acf43
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-11-06 19:03:42 +0530

    Ashok/Ajit | Allow ouput to video files

--- src/TimeLapse.cpp
+++ src/TimeLapse.cpp
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "string.h"
 #include "stdlib.h"
 #include <opencv/highgui.h>
 #include <opencv2/opencv.hpp>
@@ -8,41 +9,60 @@ int DEFAULT_TIME_INTERVAL = 1;
 
 using namespace cv;
 void writeOutputImage(Mat& image, char *folder, int fileNumber) {
-	char path[1024] = {};
-	sprintf(path, "%s/image%04d.jpg", folder, fileNumber);
-	imwrite(path, image );
+    char path[1024] = {};
+    sprintf(path, "%s/image%04d.jpg", folder, fileNumber);
+    imwrite(path, image );
 }
 
 int main( int argc, char** argv )
 {
 
-	char *outputFolder = DEFAULT_OUTPUT_FODLER;
-	int timeInterval = DEFAULT_TIME_INTERVAL;
-	int currentFileCount = 1;
+    char *outputFolder = DEFAULT_OUTPUT_FODLER;
+    int timeInterval = DEFAULT_TIME_INTERVAL;
+    int currentFileCount = 1;
+    int outputVideoFps = 24;
+    Mat frame;
+    bool writeVideo = true;
 
-	// if(argc != 2)
-	// {
-	// 	printf("Usage: <timeInterval> <outputFolder>\n");
-	// 	return -1;
-	// }
+    printf("Usage: <image|[video]> <timeInterval[1]> <outputFolder[\".\"]>\nRunning with defaults...\n");
 
-	if(argc>1) outputFolder = argv[1];
-	if(argc>2) sscanf(argv[2], "%d", &timeInterval);
+    if(argc>1) writeVideo = strcmp(argv[1],"video")==0;
+    if(argc>2) outputFolder = argv[2];
+    if(argc>3) sscanf(argv[3], "%d", &timeInterval);
 
 
-
-	VideoCapture cap(0); // open the default camera
+    //open input
+    VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
+    //open output
+    VideoWriter outputVideo;    // Open the output
+    if(writeVideo)
+    {
+        cap >> frame;               // get a new frame from camera to determine output video size
+        outputVideo.open("./videoFile.mp4", -1, outputVideoFps, frame.size(), true);
+        if (!outputVideo.isOpened())
+        {
+            printf("Could not open the output video for write.\n");
+            return -1;
+        }
+    }
     namedWindow("preview", CV_WINDOW_AUTOSIZE);
     for(;;)
     {
-        Mat frame;
+        printf("in loop\n");
         cap >> frame; // get a new frame from camera
         imshow("preview", frame);
-       	writeOutputImage(frame, outputFolder, currentFileCount++);
+        if (writeVideo)
+        {
+            outputVideo << frame;
+        }
+        else
+        {
+            writeOutputImage(frame, outputFolder, currentFileCount++);
+        }
         sleep(timeInterval);
-	}
-	return 0;
+    }
+    return 0;
 }
