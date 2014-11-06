983919290943f2a4f3001f97308fecadcb04f6a5
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-11-06 18:18:22 +0530

    Ashok/Ajit | Capture images from webcam and store to disk every 1 second

--- src/TimeLapse.cpp
+++ src/TimeLapse.cpp
@@ -1,34 +1,49 @@
+#include "stdio.h"
+#include "stdlib.h"
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/opencv.hpp>
 
+char DEFAULT_OUTPUT_FODLER[] = ".";
+int DEFAULT_TIME_INTERVAL = 1;
+
 using namespace cv;
+void writeOutputImage(Mat& image, char *folder, int fileNumber) {
+	char path[1024] = {};
+	sprintf(path, "%s/image%04d.jpg", folder, fileNumber);
+	imwrite(path, image );
+}
 
 int main( int argc, char** argv )
 {
-	char* imageName = argv[1];
 
-	Mat image;
-	image = imread( imageName, 1 );
+	char *outputFolder = DEFAULT_OUTPUT_FODLER;
+	int timeInterval = DEFAULT_TIME_INTERVAL;
+	int currentFileCount = 1;
 
-	if( argc != 2 || !image.data )
-	{
-		printf( " No image data \n " );
-		return -1;
+	// if(argc != 2)
+	// {
+	// 	printf("Usage: <timeInterval> <outputFolder>\n");
+	// 	return -1;
+	// }
+
+	if(argc>1) outputFolder = argv[1];
+	if(argc>2) sscanf(argv[2], "%d", &timeInterval);
+
+
+
+	VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    namedWindow("preview", CV_WINDOW_AUTOSIZE);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        imshow("preview", frame);
+       	writeOutputImage(frame, outputFolder, currentFileCount++);
+        sleep(timeInterval);
 	}
-
-	Mat gray_image;
-	cvtColor( image, gray_image, CV_BGR2GRAY );
-
-	imwrite( "Gray_Image.jpg", gray_image );
-
-	namedWindow( imageName, CV_WINDOW_AUTOSIZE );
-	namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
-
-	imshow( imageName, image );
-	imshow( "Gray image", gray_image );
-
-	waitKey(0);
-
 	return 0;
 }
